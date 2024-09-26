6414e04fd47f6b7c82b68783ad501d1d3520405e
001-mak <ahsankhaleeq10@gmail.com>
2024-09-26 17:23:45 +0500

    register login api

---
+++ backend/src/routes/auth.route.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import * as authController from '../controller/auth.controller'
+const authRouter = Router();
+
+authRouter.post('/register', authController.handleUserRegister);
+
+// authRouter.post('/signup', validate(signupSchema), authController.handleSignUp);
+
+authRouter.post('/login', authController.handleLogin);
+
+// authRouter.post('/logout', authController.handleLogout);
+
+// authRouter.post('/refresh', authController.handleRefresh);
+
+export default authRouter;
