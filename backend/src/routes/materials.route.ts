c9554e527b971bb6a01ac2b067940e46779f5beb
ali-shakeel880 <mayoali880@gmail.com>
2024-10-17 00:40:25 +0500

    Added material Validations

--- backend/src/routes/materials.route.ts
+++ backend/src/routes/materials.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import * as materialsController from '../controller/materials.controller';
 import isAuth from '../middleware/isAuth';
+import validate from '../middleware/validate';
+import {materialValidationSchema} from '../validations/material.validation';
 
 const materialsRouter = Router();
 
-materialsRouter.post('/', isAuth, materialsController.handleCreateMaterial);
+materialsRouter.post('/',validate({ body: materialValidationSchema}), isAuth, materialsController.handleCreateMaterial);
 
 
 materialsRouter.get('/', isAuth, materialsController.handleGetAllMaterials);
@@ -13,7 +15,7 @@ materialsRouter.get('/', isAuth, materialsController.handleGetAllMaterials);
 materialsRouter.get('/:id', isAuth, materialsController.handleGetMaterialById);
 
 
-materialsRouter.put('/:id', isAuth, materialsController.handleUpdateMaterial);
+materialsRouter.put('/:id',validate({ body: materialValidationSchema}), isAuth, materialsController.handleUpdateMaterial);
 
 
 materialsRouter.delete('/:id', isAuth, materialsController.handleDeleteMaterial);
---
+++ backend/src/validations/material.validation.ts
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+export const materialValidationSchema = Joi.object({
+  material_name: Joi.string()
+    .max(100)
+    .required()
+    .messages({
+      'string.base': 'Material name should be a string.',
+      'string.empty': 'Material name cannot be empty.',
+      'string.max': 'Material name should not exceed 100 characters.',
+      'any.required': 'Material name is required.'
+    }),
+    
+  material_description: Joi.string()
+    .max(500)
+    .optional()
+    .allow(null, '')
+    .messages({
+      'string.base': 'Material description should be a string.',
+      'string.max': 'Material description should not exceed 500 characters.'
+    }),
+
+  material_price: Joi.number()
+    .greater(0)
+    .required()
+    .messages({
+      'number.base': 'Material price must be a number.',
+      'number.greater': 'Material price must be greater than 0.',
+      'any.required': 'Material price is required.'
+    })
+});
+
+module.exports = materialValidationSchema;
